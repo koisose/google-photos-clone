fcd485357c8d5843d766ee4e9d67ec01c3c1bdbe
koisose.eth <plokotokbelajar@gmail.com>
2024-01-12 03:08:40 +0000

    💅 feat(crawl): Menambahkan pustaka Tailwind CSS v3
    Menambahkan pustaka Tailwind CSS v3, plugin Prettier, PostCSS, dan Autoprefixer untuk meningkatkan tampilan visual aplikasi.

--- packages/crawl/src/routes/index.tsx
+++ packages/crawl/src/routes/index.tsx
@@ -1,25 +1,24 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+async function whoami(){
 
+}
 export default component$(() => {
   return (
     <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
+      <div class="flex flex-col items-center justify-start p-4">
+        <h1 class="text-4xl font-bold mt-10">Gql hashnode</h1>
+      </div>
+      <div class="flex flex-col items-center mt-5">
+    
+         <input
+    class="border-input ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex  w-full max-w-md resize rounded-md border bg-white p-2 text-sm text-gray-900 shadow-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 "
+    placeholder="Put your hashnode key here"
+    type="text"
+  />
+        <button class="ring-offset-background focus-visible:ring-ring mt-4 inline-flex h-10 items-center justify-center rounded-md bg-blue-500 px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-blue-600 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:bg-blue-400 dark:hover:bg-blue-300">
+          Submit
+        </button>
+      </div>
     </>
   );
 });
-
-export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
-};
